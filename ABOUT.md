4d7da18cbce2474c052c727943ce2205a976f0d2
ViolaRobot <isaacsiebelink@gmail.com>
2018-10-26 18:20:45 +0000

    Second commit: corrected name of ABOUT file and added true main.py file

---
+++ ABOUT.md
@@ -0,0 +1,5 @@
+ABOUT
+-----
+
+This project is to be a personal GIT-like file server solution to upload
+documents to the Raspberry Pi. More to come later.
--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-ABOUT
------
+#specify environment
 
-This project is to be a personal GIT-like file server solution to upload
-documents to the Raspberry Pi. More to come later.
+import socket
+import threading
